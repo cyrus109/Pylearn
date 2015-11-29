5d0c372185eb8b560116ff7d5fd989bdc68c2cae
Master <cyrus109@gmail.com>
2015-11-29 21:48:12 +0100

    testing

--- Pylearn.py
+++ Pylearn.py
@@ -2,4 +2,12 @@ __author__ = 'Master1'
 print( "Give me a number to multiply")
 number=input()
 print('Number ' + str(number) + ' times 2 is ' + str(int(number) * 2))
+while(i):
+    print("give a number higher than 0")
+    number=input()
+    if(number>0):
+        i=1
+    else:
+        i=0
+
 
